f155fad9f00a1569812be6f7874fd1148209b801
ayushrawat001 <146158503+ayushrawat001@users.noreply.github.com>
2023-09-29 11:42:18 +0530

    Day 1st and 2nd code

---
+++ hello.py
@@ -0,0 +1,12 @@
+print(2+3)
+print("ankit ")
+print('100')
+a="ankit"
+b=60
+c=14.6
+print(a,b,c)
+# define the type of variable
+print(type(a))
+sum=b+c
+mul=b*c
+print(sum,mul)
---
+++ task1.py
@@ -0,0 +1,4 @@
+a=int(input("enter your num:"))
+b=int(input("enter your num:"))
+sum=a+b
+print(sum)
